1aadb82b81a87b84f9d095cde25e25dde40412ca
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-03-31 19:18:22 -0300

    Módulo : Criando uma API RESTful / Aula : Finalizando o tratamento das respostas

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,19 +22,18 @@ public class LivrosResources {
 	private LivrosRepository livrosRepository;
 
 	@GetMapping
-	public List<Livro> listar() {
-		return livrosRepository.findAll();
+	public ResponseEntity<List<Livro>> listar() {
+		return ResponseEntity.status(HttpStatus.OK).body(livrosRepository.findAll());
 	}
 
 	@PostMapping
 	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
 		// Salvamos o livro
 		livro = livrosRepository.save(livro);
-		
+
 		// Criou uma uri para representar a uri do objeto salvo
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id").buildAndExpand(livro.getId()).toUri();
-		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id").buildAndExpand(livro.getId()).toUri();
+
 		// Usando o build para criar o response da forma correta
 		return ResponseEntity.created(uri).build();
 	}
@@ -41,25 +41,34 @@ public class LivrosResources {
 	@GetMapping("/{id}")
 	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
 		Optional<Livro> livro = livrosRepository.findById(id);
-        
-		// se o livro nao for encontrado ele retorna status 404 
-		if (livro.isEmpty() ==  true) {
-           return ResponseEntity.notFound().build();
+
+		// se o livro nao for encontrado ele retorna status 404
+		if (livro.isEmpty() == true) {
+			return ResponseEntity.notFound().build();
 		}
-		
-        // Aqui é retornado um status = 200 e o objeto livro no corpo da requisiçao
+
+		// Aqui é retornado um status = 200 e o objeto livro no corpo da requisiçao
 		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 
 	@DeleteMapping("/{id}")
-	public void deletar(@PathVariable("id") Long id) {
-		livrosRepository.deleteById(id);
+	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+		try {
+			livrosRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return ResponseEntity.notFound().build();
+		}
+		// Retorna 204 no content
+		return ResponseEntity.noContent().build();
+
 	}
 
 	@PutMapping("/{id}")
-	public void atualizar(@RequestBody Livro livro, @PathVariable("id") Long id) {
+	public ResponseEntity<Void> atualizar(@RequestBody Livro livro, @PathVariable("id") Long id) {
 		livro.setId(id);
 		livrosRepository.save(livro);
+
+		return ResponseEntity.noContent().build();
 	}
 
 }
